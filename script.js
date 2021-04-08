d0f62e24465b486b9a8f28a516333f3b801be610
Vikram-01 <61109050+Vikram-01@users.noreply.github.com>
2021-04-08 09:09:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+	let str = "I'm VIKRAM.";
+	let i = 0;
+	let curr = '';
+	typeWriter();
+	function typeWriter() {
+		curr += str.charAt(i);
+		document.getElementById('name').innerHTML = curr + '|';
+		i++;
+		if (i >= str.length) {
+			curr = '';
+			i = 0;
+				timeout = setTimeout(function () {
+        console.log('Input Value:', textInput.value);
+    }, 1000);		} else setTimeout(typeWriter, 100);
+	}
+};
